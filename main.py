1a358a798a1c5e3af3c79d1c39d40d982f176fb1
tarasovEgor <Jeglederefter@yandex.ru>
2025-06-08 18:19:26 +0300

    main.py modified

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 import pandas as pd
 import os
-import csv
 import multiprocessing
+
+from tqdm import tqdm
 from workers import scrape_company_task
 
-
 def main():
-    df = pd.read_excel('src/templates/input_links_building.xlsx', sheet_name='Лист1')
+    df = pd.read_excel('src/templates/sample.xlsx', sheet_name='Лист1')
+    keywords_df = pd.read_excel('src/templates/keywords.xlsx', sheet_name='Лист2')
+
+    keywords_eng = set(keywords_df['keyword_eng'].dropna().str.lower())
+    keywords_ru = set(keywords_df['keyword_ru'].dropna().str.lower())
+
     output_dir = 'downloaded_data'
     os.makedirs(output_dir, exist_ok=True)
 
-    with open('scraped_output_building.csv', 'w', newline='', encoding='utf-8') as f:
-        writer = csv.writer(f)
-        writer.writerow(['company', 'url', 'INN', 'web_page_text'])
-
-    lock = multiprocessing.Manager().Lock()
+    manager = multiprocessing.Manager()
+    lock = manager.Lock()
 
     company_args = [
-        (row['company'], row['url'], row['INN'], output_dir, lock)
+        (row['company'], row['url'], row['INN'], output_dir, lock, keywords_eng, keywords_ru)
         for _, row in df.iterrows()
     ]
 
     with multiprocessing.Pool(processes=4) as pool:
-        pool.map(scrape_company_task, company_args)
-
+        list(tqdm(pool.imap_unordered(scrape_company_task, company_args), total=len(company_args)))
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
