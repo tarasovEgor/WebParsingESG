c457f937d08070cd39d448792892e09812219aee
tarasovEgor <Jeglederefter@yandex.ru>
2025-07-06 18:19:54 +0300

    main.py updated to containg the new PDF-file parsing module

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import multiprocessing
 from tqdm import tqdm
 from workers import scrape_company_task
 
+PDF_DOWNLOAD_ENABLED = True
+
 def main():
     df = pd.read_excel('src/templates/sample.xlsx', sheet_name='Лист1')
     keywords_df = pd.read_excel('src/templates/keywords.xlsx', sheet_name='Лист2')
@@ -19,9 +21,9 @@ def main():
     lock = manager.Lock()
 
     company_args = [
-        (row['company'], row['url'], row['INN'], output_dir, lock, keywords_eng, keywords_ru)
+        (row['company'], row['url'], row['INN'], output_dir, lock, keywords_eng, keywords_ru, PDF_DOWNLOAD_ENABLED)
         for _, row in df.iterrows()
-    ]
+    ]   
 
     with multiprocessing.Pool(processes=4) as pool:
         list(tqdm(pool.imap_unordered(scrape_company_task, company_args), total=len(company_args)))
